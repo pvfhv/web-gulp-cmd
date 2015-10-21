35bcd499c15c0c20c6d2477cddd749b2d45a3196
安 <pvfhv@qq.com>
2015-10-21 18:11:19 +0800

    template
    fix

--- config.rb
+++ config.rb
@@ -3,10 +3,11 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "styles"
+css_dir = "dist/css"
 sass_dir = "sass"
 images_dir = "images"
-javascripts_dir = "javascripts"
+javascripts_dir = "scripts"
+sourcemap = true
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,43 @@
 
 var gulp = require('gulp');
 var compass = require('gulp-compass');
+var jade = require('gulp-jade');
 var config = {
     'sass': 'sass/**/*.scss',
-    'css': 'dist/css'
+    'distCss': 'dist/css'
 };
 
-
+//样式
 gulp.task('styles', function () {
     gulp.src(config.sass)
         .pipe(compass({
             config_file: 'config.rb',
-            css: config.css,
+            css: config.distCss,
             sass: 'sass'
         }))
-        .pipe(gulp.dest(config.css));
+        .pipe(gulp.dest(config.distCss));
 });
+
+//copy fonts
+gulp.task('copyFont',function(){
+    var src='node_modules/bootstrap-sass/assets/fonts/bootstrap/*';
+    var dest= config.distCss+'/fonts/';
+
+    return gulp.src(src)
+        .pipe(gulp.dest(dest));
+});
+
+//template
+gulp.task('templates', function() {
+    var YOUR_LOCALS = {};
+
+    gulp.src('./template/*.jade')
+        .pipe(jade({
+            doctype:'html',
+            pretty:true
+        }))
+        .pipe(gulp.dest('./dist/'))
+});
+
+
+
