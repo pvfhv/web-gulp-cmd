58e2b33a380d285ff163bbdb531c278bfae43074
安 <pvfhv@qq.com>
2015-11-11 18:58:07 +0800

    es6 React
    es6 React

--- scripts/test/reactTest.js
+++ scripts/test/reactTest.js
@@ -200,3 +200,57 @@ let oFirst = $('#first').get(0);
 //});
 //
 //ReactDOM.render(<Hello />,oFirst);
+
+//计时器
+class Hello extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={'nCount':0};
+    }
+
+    tick(){
+        this.setState({nCount:this.state.nCount+1});
+    }
+
+    componentDidMount(){
+        this.timer=setInterval(this.tick.bind(this),1000);
+    }
+
+    componentWillUnMount(){
+        clearInterval(this.timer);
+    }
+
+    render(){
+        return (
+            <div>经过{this.state.nCount}秒！</div>
+        );
+    }
+}
+
+//var  Hello = React.createClass({
+//    getInitialState:function(){
+//        return {
+//            nCount:0
+//        }
+//    },
+//
+//    tick:function(){
+//        this.setState({nCount:this.state.nCount+1});
+//    },
+//
+//    componentDidMount:function(){
+//        this.timer=setInterval(this.tick,1000);
+//    },
+//
+//    componentWillUnMount:function(){
+//        clearInterval(this.timer);
+//    },
+//
+//    render:function(){
+//        return (
+//            <div>经过{this.state.nCount}秒！</div>
+//        );
+//    }
+//});
+
+ReactDOM.render(<Hello />,oFirst);
