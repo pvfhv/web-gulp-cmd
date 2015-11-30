d88f94eb69347bc0e7d5c3cc6804139183ac2b37
安 <pvfhv@qq.com>
2015-11-30 18:00:29 +0800

    fix
    fix

--- scripts/test/reactVariable.js
+++ scripts/test/reactVariable.js
@@ -2,27 +2,37 @@
  * Created by Administrator on 2015/11/29.
  */
 
-var data = {
-    var1: 'var1',
-    var2: 'var2'
-};
+var data = [{"name":"变量1","value": "var1"},{"name":"变量2","value":"var2"}];
 
 class VariableView extends React.Component{
     constructor(props){
         super(props);
-        this.state=this.props;
+        this.state={
+            "var1":this.props[0],
+            "var2":this.props[1]
+        };
     }
 
-    handleChange(json){
-        this.setState(json);
+    handleChange(key,json){
+        var oldState=this.state;
+        var othObj=_.omit(oldState,key);
+
+        for(var k in othObj){
+            if(othObj[k].value==json.value){
+                alert('重名了~~');
+                return;
+            }
+        }
+
+        oldState[key]=json;
+        this.setState(oldState);
     }
 
     render(){
         var rows=[];
 
-        for(var key in this.props){
-            var row={key:this.props[key]};
-            rows.push(<VariableRowView value={this.props[key]} key={key} handleChange={this.handleChange.bind(this)} />);
+        for(var key in this.state){
+            rows.push(<VariableRowView row={this.state[key]} index={key} key={key} handleChange={this.handleChange.bind(this)} />);
         }
 
         return (
@@ -34,9 +44,17 @@ class VariableView extends React.Component{
 }
 
 class VariableRowView extends React.Component{
+    handleChange(){
+        var row=this.props.row;
+        var v =this.refs.tempVar.value;
+        row.value=v;
+
+        this.props.handleChange(this.props.index,row);
+    }
+
     render(){
         return (
-            <input type="text" value={this.props.value} onChange={this.props.handleChange.bind(this)} />
+            <input type="text" ref="tempVar" value={this.props.row.value} onChange={this.handleChange.bind(this)} />
         );
     }
 }
