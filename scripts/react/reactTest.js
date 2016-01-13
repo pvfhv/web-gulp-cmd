cf3528c0eee630d3eddc03ba54a48f053fdb2b4a
安 <pvfhv@qq.com>
2016-01-14 00:24:49 +0800

    fix
    fix

--- scripts/react/reactTest.js
+++ scripts/react/reactTest.js
@@ -28,7 +28,12 @@ export default class ReactTest {
         //this.four();
         //this.five();
         //this.six();
-        this.seven();
+        //this.seven();
+        //this.eight();
+        //this.nine();
+        //this.ten();
+        //this.eleven();
+        this.twelve();
     }
 
     first() {
@@ -112,8 +117,146 @@ export default class ReactTest {
             }
         });
 
-
         ReactDOM.render(<HelloMsg />, oFirst);
+    }
+
+    eight(){
+        let oFirst = $('#first').get(0);
+        //类名首字母要大写
+        let MyComponent = React.createClass({
+            handleClick: function () {
+                this.refs.myTextInput.focus();
+            },
+            handleMouseDown: function (e) {
+                this.refs.myTextInput.value = e.clientX;
+            },
+            handleWheel: function (e) {
+                this.refs.myTextInput.value = e.deltaY;
+            },
+            render: function () {
+                return (
+                    <div>
+                        <input type="text" ref="myTextInput" onMouseDown={this.handleMouseDown}
+                               onWheel={this.handleWheel}/>
+                        <input type="button" onClick={this.handleClick} value="获得焦点"/>
+                    </div>
+                );
+            }
+        });
+
+        ReactDOM.render(<MyComponent />, oFirst);
+    }
+
+    nine(){
+        let oFirst = $('#first').get(0);
+        //state
+        var MyComponent = React.createClass({
+            getInitialState: function () {
+                return {
+                    liked: false,
+                    count: 0
+                };
+            },
+            handleClick: function () {
+                this.setState({liked: !this.state.liked, count: this.state.count + 1});
+            },
+            render: function () {
+                var text = this.state.liked ? "like" : "have\'t liked";
+                return (
+                    <div onClick={this.handleClick}>
+                        you {text} this,click count {this.state.count};
+                    </div>
+                );
+            }
+        });
+
+        ReactDOM.render(<MyComponent />, oFirst);
+
+    }
+
+    ten(){
+        let oFirst = $('#first').get(0);
+        var MyComponent = React.createClass({
+            getInitialState: function () {
+                return {
+                    value: 'hello'
+                }
+            },
+            handleChange: function (e) {
+                this.setState({value: e.target.value});
+            },
+            render: function () {
+                //var sHtml=$('#ReactTemplate').html();
+                return (
+                    //<div>{sHtml}</div>
+                    <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                );
+            }
+        });
+
+        ReactDOM.render(<MyComponent />, oFirst);
+    }
+
+    eleven(){
+        let oFirst = $('#first').get(0);
+        let Hello = React.createClass({
+            getInitialState: function () {
+                return {
+                    opacity: 1
+                };
+            },
+            componentDidMount: function () {
+                this.timer = setInterval(function () {
+                    var opacity = this.state.opacity;
+                    opacity -= 0.05;
+
+                    if (opacity <= 0) {
+                        opacity = 1;
+                    }
+
+                    this.setState({
+                        opacity: opacity
+                    });
+                }.bind(this), 100);
+            },
+            render: function () {
+                return (
+                    <div style={{opacity:this.state.opacity}}>Hello {this.props.name}</div>
+                );
+            }
+        });
+
+        ReactDOM.render(<Hello name="world"/>, oFirst);
+    }
+
+    twelve(){
+        let oFirst = $('#first').get(0);
+        //ajax
+        let Hello = React.createClass({
+            getInitialState: function () {
+                return {
+                    name: 'tom',
+                    age: 10
+                };
+            },
+            componentDidMount: function () {
+                $.getJSON('../simulates/PersonData.json', function (oData) {
+                    if (this.isMounted()) {
+                        this.setState({
+                            name: oData.name + '_' + Date.now(),
+                            age: oData.age
+                        })
+                    }
+                }.bind(this));
+            },
+            render: function () {
+                return (
+                    <div>姓名：{this.state.name}，年龄：{this.state.age}</div>
+                );
+            }
+        });
+
+        ReactDOM.render(<Hello />, oFirst);
     }
 }
 
@@ -122,140 +265,3 @@ class HelloMsg extends React.Component {
         return <h1>hello,{this.props.name}</h1>;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-//类名首字母要大写
-//var MyComponent = React.createClass({
-//    handleClick:function(){
-//        this.refs.myTextInput.focus();
-//    },
-//    handleMouseDown:function(e){
-//        this.refs.myTextInput.value=e.clientX;
-//    },
-//    handleWheel:function(e){
-//        this.refs.myTextInput.value=e.deltaY;
-//    },
-//    render:function(){
-//        return (
-//            <div>
-//                <input type="text" ref="myTextInput" onMouseDown={this.handleMouseDown} onWheel={this.handleWheel} />
-//                <input type="button" onClick={this.handleClick} value="获得焦点" />
-//            </div>
-//        );
-//    }
-//});
-//
-//ReactDOM.render(<MyComponent />,oFirst);
-
-
-//state
-//var MyComponent = React.createClass({
-//    getInitialState: function () {
-//        return {
-//            liked: false,
-//            count:0
-//        };
-//    },
-//    handleClick: function () {
-//        this.setState({liked: !this.state.liked,count:this.state.count+1});
-//    },
-//    render:function(){
-//        var text=this.state.liked?"like":"have\'t liked";
-//        return (
-//            <div onClick={this.handleClick}>
-//                you {text} this,click count {this.state.count};
-//            </div>
-//        );
-//    }
-//});
-//
-//ReactDOM.render(<MyComponent />,oFirst);
-
-//var MyComponent=React.createClass({
-//    getInitialState:function(){
-//        return {
-//            value:'hello'
-//        }
-//    },
-//    handleChange:function(e){
-//        this.setState({value:e.target.value});
-//    },
-//    render:function(){
-//        //var sHtml=$('#ReactTemplate').html();
-//        return (
-//            //<div>{sHtml}</div>
-//            <input type="text" value={this.state.value} onChange={this.handleChange} />
-//        );
-//    }
-//});
-//
-//
-//ReactDOM.render(<MyComponent />,oFirst);
-
-
-//var Hello=React.createClass({
-//    getInitialState:function(){
-//        return {
-//            opacity:1
-//        };
-//    },
-//    componentDidMount:function(){
-//        this.timer=setInterval(function(){
-//            var opacity=this.state.opacity;
-//            opacity-=0.05;
-//
-//            if(opacity<=0){
-//                opacity=1;
-//            }
-//
-//            this.setState({
-//                opacity:opacity
-//            });
-//        }.bind(this),100);
-//    },
-//    render:function(){
-//        return (
-//            <div style={{opacity:this.state.opacity}}>Hello {this.props.name}</div>
-//        );
-//    }
-//});
-//
-//ReactDOM.render(<Hello name="world" />,oFirst);
-
-//ajax
-//var Hello=React.createClass({
-//    getInitialState:function(){
-//        return {
-//            name:'tom',
-//            age:10
-//        };
-//    },
-//    componentDidMount:function(){
-//        $.get('../simulates/PersonData.json',function(oData){
-//             if(this.isMounted()){
-//                 this.setState({
-//                     name:oData.name+'_'+Date.now(),
-//                     age:oData.age
-//                 })
-//             }
-//        }.bind(this),'json');
-//    },
-//    render:function(){
-//        return (
-//            <div>姓名：{this.state.name}，年龄：{this.state.age}</div>
-//        );
-//    }
-//});
-//
-//ReactDOM.render(<Hello />,oFirst);
-
-
