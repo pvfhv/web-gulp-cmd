27bf3d62ac93bc2f69ed09cfbe1dd3c1cc31ce32
安 <pvfhv@qq.com>
2016-03-23 18:56:28 +0800

    fix
    fix

--- scripts/react/Lifecycle.js
+++ scripts/react/Lifecycle.js
@@ -66,14 +66,36 @@ var Component = React.createClass({
         //console.log(e.key);//Alt shift。。
         //console.log(e.locale);
         //console.log(e.repeat);
-        console.log(e.which);
+        //console.log(e.which);
     },
     _handleFocus:function(e){
         console.log(e.DOMEventTarget);
+        console.log(e.relatedTarget);
     },
     _handleDragStart:function(e){
-        console.log(e.clientX);
-        e.preventDefault();
+        var disX = e.pageX - $(e.currentTarget).offset().left;
+        var disY = e.pageY - $(e.currentTarget).offset().top;
+        $(e.currentTarget).data('initPos',{disX:disX,disY:disY});
+    },
+    _handleDragging:function(e){
+        var oPos = $(e.currentTarget).data('initPos');
+        $(e.currentTarget).css({left:e.pageX - oPos.disX,top:e.pageY - oPos.disY});
+    },
+    _handleDragEnd:function(e){
+        var oPos = $(e.currentTarget).position();
+        $(e.currentTarget).data('initPos',{left:oPos.left,top:oPos.top});
+    },
+    _handleSelect:function(e){
+        console.log('select');
+    },
+    _handleScroll:function(e){
+        console.log(e.detail+',target='+e.currentTarget+',view='+e.view);
+    },
+    _handleWheel:function(e){
+        console.log(`deltaMode=${e.deltaMode},e.deltaX=${e.deltaX},e.deltaY=${e.deltaY},e.deltaZ={e.deltaZ}`);
+    },
+    _handleTransition:function(e){
+        console.log(`propertyName:${e.propertyName},pseudoElement:${e.pseudoElement},elapsedTime:${e.elapsedTime}`)
     },
     render: function () {
         return (
@@ -83,7 +105,8 @@ var Component = React.createClass({
                 <input type="text" onFocus={this._handleFocus} onKeyDown={this._handleKeyDown} /><br />
                 <input type="button" value="移除组件" onClick={this.removeComponent}/><br />
                 <input type="button" value="重新渲染" onClick={this.rerender}/>
-                <div id="dragdiv" onDragStart={this._handleDragStart}>我可以拖动</div>
+                <div id="dragdiv" onScroll={this._handleScroll} onWheel={this._handleWheel} draggable="true" onDragStart={this._handleDragStart} onDrag={this._handleDragging} onDragEnd={this._handleDragEnd} onTransitionEnd={this._handleTransition}>我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动我可以拖动</div>
+                <input type="text" onSelect={this._handleSelect} defaultValue="测试一下选中文字" />
             </div>
         );
     }
